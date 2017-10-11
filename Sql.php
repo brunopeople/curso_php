db38d64ec005f690b3b2d8e80dd4c97080d7138a
brunopeople <brunopessoa.melo@gmail.com>
2017-10-11 12:23:59 -0300

    iniciamos o projeto

---
+++ Sql.php
@@ -0,0 +1,47 @@
+<?php
+
+class Sql extends PDO {
+
+	private $con;
+
+	public function __construct(){
+		$this-> con = new PDO("mysql:host=localhost;dbname=db_php7","root","root");
+	}
+
+	private function setParams($statement,$parameters = array()){
+		foreach ($parameters as $key => $value) {
+			
+			$stmt->bindParam($key,$value);
+		}
+	}
+
+	private function setParam($statement,$key,$value){
+
+		$statement->bindParam($key,$value);
+	}
+
+
+	public function query($rawQuery,$params = array()){
+		
+		$stmt = $this->con->prepare($rawQuery);
+
+		$this->setParams($stmt,$params);
+
+		$stmt->execute();
+
+	    return $stmt;
+		}
+
+		public function select($rawQuery,$params = array()):array
+
+		{
+
+			$stmt = $this->query($rawQuery, $params);
+
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+
+	}
+ 
+
+?>
